1afa57405ffbe5ad24a95a245f9bd4c358bc935e
Avinash20Kumar <Avinashkumarprajapati@gmail.com>
2017-02-21 12:39:48 +0530

    How to parse json using ajax from any url

---
+++ Ajax.js
@@ -0,0 +1,68 @@
+
+var pageCounter =1;
+var animalinfo = document.getElementById("animal-info");
+var btn = document.getElementById("btn");
+
+btn.addEventListener("click",function(){
+	var ourRequest = new XMLHttpRequest();
+
+ourRequest.open('GET','https://learnwebcode.github.io/json-example/animals-'+pageCounter+'.json');
+
+ourRequest.onload = function(){
+
+// console.log(ourRequest.responseText);
+var data = JSON.parse(ourRequest.responseText);
+// console.log(data[0]);
+renderHTML(data);
+
+};
+ourRequest.send();
+pageCounter++;
+if(pageCounter>3){
+// 	btn.classList("hide-Me");
+}
+});
+
+
+function renderHTML(data){
+
+	var htmlString= "";
+
+	for(i = 0;i<data.length;i++){
+htmlString +="<p>" +data[i].name +" is a "+ data[i].species ;
+
+
+for(j=0;j<data[i].foods.likes.length;j++){
+if(j==0){
+		htmlString += " likes " + data[i].foods.likes[j];
+}
+else{
+		htmlString += " and  "+data[i].foods.likes[j];
+}
+	
+
+	
+}
+// htmlString +=".</p>"
+htmlString += "and dislikes ";
+
+for(j=0;j<data[i].foods.dislikes.length;j++){
+if(j==0){
+		htmlString +=  data[i].foods.dislikes[j];
+}
+else{
+		htmlString += " and "+data[i].foods.dislikes[j];
+}
+	
+
+	
+}
+htmlString +=".</p>"
+//animalinfo.insertAdjacentHTML('beforeend',htmlString);
+
+	}
+// animalinfo.insertAdjacentHTML('deforeend','testing asdf');
+animalinfo.insertAdjacentHTML('beforeend',htmlString);
+
+
+}
